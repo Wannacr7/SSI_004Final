c440f563c1f5c2a7dad9f14c6ad13ae13d6d2dc6
Wannacr7 <88002544+Wannacr7@users.noreply.github.com>
2021-11-20 00:32:56 -0500

    Parte2

--- Assets/Scripts/BulletVelocity.cs
+++ Assets/Scripts/BulletVelocity.cs
@@ -6,14 +6,28 @@ public class BulletVelocity : MonoBehaviour
 {
     public Vector2 velocity;
     private float mass = .5f, g = -9.8f, Ka = -0.2f;
-    private void Start()
-    {
-        
-    }
+
     void Update()
     {
         transform.position += new Vector3(velocity.x, velocity.y, 0) * Time.deltaTime;
         velocity+= new Vector2(Ka*velocity.x, mass*g+ Ka * velocity.y) * Time.deltaTime;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Target"))
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+        if (col.CompareTag("Obstacle"))
+        {
+            velocity = -velocity;
+        }
+        if (col.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
